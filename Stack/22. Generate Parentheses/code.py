0d1b3b6397730583aaad7c0894089c3ff711554d
James <44154584+yuxundu@users.noreply.github.com>
2023-05-07 17:03:29 -0400

    22. Generate Parentheses

---
+++ Stack/22. Generate Parentheses/code.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        res = []
+
+
+        def backtrack(openN, closeN):
+            if openN == n and closeN == n:
+                print(stack)
+                res.append("".join(stack))
+                return
+            if openN < n:
+                stack.append("(")
+                backtrack(openN+1, closeN)
+                stack.pop()
+            if closeN < openN:
+                stack.append(")")
+                backtrack(openN, closeN+1)
+                stack.pop()
+
+        backtrack(0,0)
+        return res
