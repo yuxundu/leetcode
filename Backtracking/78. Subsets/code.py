e4c9a4811a369c7fbdae5a3a23e465a0bac62073
James <44154584+yuxundu@users.noreply.github.com>
2023-06-04 10:29:02 -0400

    78. Subsets

---
+++ Backtracking/78. Subsets/code.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+        def dfs(index):
+            if index >= len(nums):
+                res.append(subset.copy())
+                return
+            subset.append(nums[index])
+            dfs(index+1)
+            subset.pop()
+            dfs(index+1)
+        dfs(0)
+
+        return res
