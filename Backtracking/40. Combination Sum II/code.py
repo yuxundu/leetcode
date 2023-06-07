b46ece660a3ef2cd13bf6345fca32d60e4686a3d
James <44154584+yuxundu@users.noreply.github.com>
2023-06-06 22:01:54 -0400

    40. Combination Sum II

---
+++ Backtracking/40. Combination Sum II/code.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+        def dfs(i,cur,total):
+            if total == target:
+                res.append(cur[:])
+                return
+            if i >= len(candidates) or total > target:
+                return
+            cur.append(candidates[i])
+            dfs(i+1,cur,total+candidates[i])
+            cur.pop()
+            while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
+                i += 1
+            dfs(i+1,cur,total)
+        dfs(0,[],0)
+        return res
