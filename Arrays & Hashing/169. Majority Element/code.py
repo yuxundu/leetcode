d27dd0dfc9909149e82fd1d951dcc814464f704c
James <44154584+yuxundu@users.noreply.github.com>
2023-07-07 15:04:11 -0400

    169. Majority Element

---
+++ Arrays & Hashing/169. Majority Element/code.py
@@ -0,0 +1,8 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        res, count = 0, 0
+        for n in nums:
+            if count == 0:
+                res = n
+            count += 1 if res == n else -1
+        return res
